5f9283643496446725dfcf930c2fea2b1a8c7daf
Frank Wall <fw@moov.de>
2021-10-06 00:06:43 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# freebsd-foreman
-Deploy FreeBSD systems with Foreman
+#### Table of Contents
+
+1. [About](#about)
+1. [Requirements](#requirements)
+1. [Provision Instructions](#provision-instructions)
+1. [Build Instructions](#build-instructions)
+1. [Acknowledgement](#acknowledgement)
+
+## About
+
+This repository contains everything needed to build a customized [mfsBSD](https://mfsbsd.vx.sk/) image that can be used to provision [FreeBSD](https://www.freebsd.org/) servers with [Foreman](https://theforeman.org/).
+
+## Requirements
+
+In order to deploy FreeBSD servers with Foreman, the following requirements must be met:
+
+* Provisioning templates for FreeBSD are already included in Foreman
+* A custom mfsBSD image, can be build manually (see below) or downloaded from here
+* A working PXE/DHCP/TFTP boot environment (Foreman Smart Proxy)
+* Server or VM needs at least 1 GB of RAM to store the image during installation
+
+## Provision Instructions
+
+* Create a "installation media" entry in Foreman for FreeBSD (use local or official mirror, i.e. `http://ftp.freebsd.org/pub/FreeBSD/releases/$arch/$major.$minor-RELEASE/`)
+* Create a new "operating system" entry in Foreman for the desired FreeBSD release
+* Copy the custom mfsBSD image to your TFTP server
+* Create a new host in Foreman, watch the FreeBSD installation begin :)
+
+## Build Instructions
+
+Use `build.sh` to create your own images or to include a modified version of `rc.local`. The script must be run as root (on a FreeBSD server).
+
+```
+# git clone git@github.com:fraenki/foreman-freebsd.git
+# cd foreman-freebsd
+# ./build.sh
+```
+
+## Acknowledgement
+
+Martin Matuška for creating [mfsBSD](https://mfsbsd.vx.sk/) and his talk on [Deploying FreeBSD systems with Foreman](https://blog.vx.sk/archives/60).
---
+++ build.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+ORIG_DIR=$PWD
+TMP_DIR='/tmp/image'
+IMAGE_MD_NO=99
+IMAGE_MOUNT_DIR='/mnt/loop'
+ROOT_MD_NO=100
+ROOT_MOUNT_DIR='/mnt/mfsroot'
+
+cleanup() {
+  cd $TMP_DIR
+  rm -R ${TMP_DIR}/rw $IMAGE_MOUNT_DIR $ROOT_MOUNT_DIR
+}
+
+# handle arguments
+while [ ${#} -gt 0 ]; do
+  case "${1}" in
+  -r)
+    if ! echo ${2} | grep -qE '^[0-9]+\.[0-9]+$'; then
+      echo "Invalid FreeBSD release specified"
+      exit 1
+    fi
+    RELEASE="$2"
+    _tmp=`echo $2 | sed 's/\([0-9]*.[0-9]*\).*/\1/'`
+    REL_MAJOR=${_tmp%.[0-9]*}
+    REL_MINOR=${_tmp#[0-9]*.}
+    shift
+    ;;
+  esac
+  shift 1
+done
+
+if [ ! -e "${ORIG_DIR}/rc.local" ]; then
+  echo "Custom rc.local not found."
+  exit 1
+fi
+
+if [ -z "${REL_MAJOR}" -o -z "${REL_MINOR}" ]; then
+  echo "Please specify a FreeBSD release."
+  exit 1
+fi
+
+if [ -e "/dev/md${IMAGE_MD_NO}" -o -e "/dev/md${ROOT_MD_NO}" ]; then
+  echo "Configured memory disk is already in use."
+  exit 1
+fi
+
+# generate source and target filenames
+MFSBSD_IMAGE="mfsbsd-${REL_MAJOR}.${REL_MINOR}-RELEASE-amd64.img"
+FOREMAN_IMAGE="FreeBSD-x86_64-${REL_MAJOR}.${REL_MINOR}-mfs.img"
+
+mkdir -p $TMP_DIR
+cd $TMP_DIR
+
+# download mfsbsd image
+if ! fetch https://mfsbsd.vx.sk/files/images/${REL_MAJOR}/amd64/${MFSBSD_IMAGE}; then
+  echo "Failed to download mfsbsd image"
+  exit 1
+fi
+
+# mount mfbsd disk image
+mkdir -p $IMAGE_MOUNT_DIR $ROOT_MOUNT_DIR
+mdconfig -a -t vnode -u $IMAGE_MD_NO -f $MFSBSD_IMAGE
+mount /dev/md${IMAGE_MD_NO}p2 $IMAGE_MOUNT_DIR
+
+# mount root filesystem
+cd $IMAGE_MOUNT_DIR
+cp -p mfsroot.gz $TMP_DIR
+cd $TMP_DIR
+gzip -d mfsroot.gz
+mdconfig -a -t vnode -u $ROOT_MD_NO -f mfsroot
+mount /dev/md${ROOT_MD_NO} $ROOT_MOUNT_DIR
+
+# insert rc.local into root filesystem
+echo "Customizing image... (please wait)"
+cd $ROOT_MOUNT_DIR
+cp -p root.txz $TMP_DIR
+cd $TMP_DIR
+tar Jxf root.txz
+cp -v ${ORIG_DIR}/rc.local rw/etc/rc.local
+rm root.txz
+tar cfJ root.txz rw
+mv root.txz $ROOT_MOUNT_DIR
+umount $ROOT_MOUNT_DIR
+mdconfig -d -u $ROOT_MD_NO
+gzip mfsroot
+mv mfsroot.gz $IMAGE_MOUNT_DIR
+umount $IMAGE_MOUNT_DIR
+mdconfig -d -u $IMAGE_MD_NO
+
+# finally rename to comply with what Foreman expects
+mv $MFSBSD_IMAGE $FOREMAN_IMAGE
+
+cleanup
+echo "Image stored in ${TMP_DIR}/${FOREMAN_IMAGE}"
+exit 0
